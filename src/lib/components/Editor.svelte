0d0b0d50e92c342542cf8ef512b42b572038104c
Sam N <78718829+TetraTsunami@users.noreply.github.com>
2026-02-03 11:43:27 -0600

    Initial commit

---
+++ src/lib/components/Editor.svelte
@@ -0,0 +1,3 @@
+<div class="h-full flex flex-col items-center justify-center text-center">
+
+</div>
---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import DNMovieCamera from '~icons/dinkie-icons/movie-camera';
+</script>
+
+<aside class="w-80 shrink-0 border-r border-zinc-800 bg-zinc-900 flex flex-col h-full">
+  <div class="px-6 py-4 flex items-center justify-between border-b border-zinc-800 h-18 from-green-950 to-zinc-900 bg-linear-to-br">
+        <div class="flex items-center gap-3">
+          <div class="w-8 h-8 rounded-lg bg-green-900 border border-green-700 flex items-center justify-center">
+            <DNMovieCamera class="w-4 h-4 text-green-400" />
+          </div>
+          <h1 class="font-semibold text-sm">SimpleGIF</h1>
+        </div>
+      </div>
+</aside>
