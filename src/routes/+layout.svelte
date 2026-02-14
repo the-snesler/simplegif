153387c959005da2e69b6589e188a3031fe2eda1
Sam N <78718829+TetraTsunami@users.noreply.github.com>
2026-02-13 23:48:13 -0600

    Fix site title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,8 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>SimpleGIF - Local GIF toolkit</title>
+</svelte:head>
 {@render children()}
